2c39eba81046f15ab541ef4bc446cbb3a92fdd2b
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-10-11 02:50:50 -0600

    Change App.js to class & Using State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-    return (
-        <div className="App" >
-            <h1 > Hi, I 'm a React App</h1>
-            <p>This is really working! </p>
-            <Person name="José" age="25" />
-            <Person name="Carla" age="25" >My Hobbies: Racing </Person>
-            <Person name="Ramón" age="25" />
-        </div>
-    );
+class App extends Component {
+    state = {
+        persons: [
+            { name: 'José', age: 25 },
+            { name: 'Carla', age: 25 },
+            { name: 'Ramón', age: 28 }
+        ]
+    }
 
+    render() {
+        return (
+            <div className="App" >
+                <h1 > Hi, I 'm a React App</h1>
+                <p>This is really working! </p>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing </Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            </div>
+        );
+    }
     //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi I\'m a React App!!!'));
 }
 
