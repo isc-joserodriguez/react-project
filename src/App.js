47429417418ce0f11bf6ee6969ff47642aba1394
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-10-11 02:29:26 -0600

    Children Prop

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <h1 > Hi, I 'm a React App</h1>
             <p>This is really working! </p>
             <Person name="José" age="25" />
-            <Person name="Carla" age="25" />
+            <Person name="Carla" age="25" >My Hobbies: Racing </Person>
             <Person name="Ramón" age="25" />
         </div>
     );
