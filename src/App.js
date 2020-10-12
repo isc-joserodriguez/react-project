4d344b312e0181df94ca116b6e97e5de16b66258
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-10-12 02:22:34 -0600

    Adding Input & onChange Event

--- src/App.js
+++ src/App.js
@@ -26,20 +26,31 @@ class App extends Component {
 
     }
 
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                { name: 'José Antonio', age: 25 },
+                { name: event.target.value, age: 25 },
+                { name: 'Ramón', age: 26 }
+            ]
+        });
+    }
+
     //ADD render (){return JSX};
     render() {
         return (
             <div className="App" >
                 <h1 > Hi, I 'm a React App</h1>
                 <p>This is really working! </p>
-                <button onClick={()=>this.switchNameHandler('José Antonio')}>Switch Name</button>
+                <button onClick={() => this.switchNameHandler('José Antonio')}>Switch Name</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age} />
                 <Person
                     name={this.state.persons[1].name}
-                    age={this.state.persons[1].age} 
-                    click={this.switchNameHandler.bind(this,'Carla')}>My Hobbies: Racing </Person>
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Carla')}
+                    changed={this.nameChangedHandler}>My Hobbies: Racing </Person>
                 <Person
                     name={this.state.persons[2].name}
                     age={this.state.persons[2].age} />
