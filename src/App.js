6431ac5123b90437df4835dfb68173a9a19c32fd
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-10-28 17:46:23 -0700

    Adding and Using Styled Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 //function App() {
@@ -93,7 +92,6 @@ class App extends Component {
 
 
         return (
-            <StyleRoot>
                 <div className="App" >
                     <h1 > Hi, I 'm a React App</h1>
                     <p className={classes.join(' ')}>This is really working! </p>
@@ -103,12 +101,11 @@ class App extends Component {
 
                     {persons}
                 </div>
-            </StyleRoot>
         );
     };
     //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi I\'m a React App!!!'));
 };
 
-export default Radium(App);
+export default App;
 
 
