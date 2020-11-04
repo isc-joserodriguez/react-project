e1ec9c0db108373d3ab863559e7dcab8ed62bd8c
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-11-04 12:39:25 -0700

    Using Error Boundaries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,9 @@ import React from 'react';
 import classes from './Person.module.css';
 
 const person = (props) => {
+    if (Math.random() > 0.7) {
+        throw new Error('Something went wrong');
+    }
     return (
         <div className={classes.Person}>
             < p onClick={props.click} > I'm {props.name} and I am {props.age} years old!!</p>
